e3e9d67bd95476ae7f3b4de238e498c24a064c1a
PanderOpenTaal <pander@opentaal.org>
2013-01-14 10:39:30 +0100

    Correctie en meervoud gemaakt.

---
+++ correctie.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="content-type" content="text/html;charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>
+        </title>
+        <link rel="stylesheet" href="jquery.mobile-1.2.0.min.css" />
+        <link rel="stylesheet" href="my.css" />
+        <style>
+            /* App custom styles */
+        </style>
+        <script src="jquery.min.js">
+        </script>
+        <script src="jquery.mobile-1.2.0.min.js">
+        </script>
+        <script src="my.js">
+        </script>
+    </head>
+    <body OnLoad="document.bijdrage.correctie.focus();">
+        <!-- Home -->
+        <div data-role="page" id="page1">
+            <div data-role="content">
+                <div data-role="collapsible-set" data-theme="b">
+                    <div data-role="collapsible" data-collapsed="false">
+                        <h3>
+                            Correctie bijdragen
+                        </h3>
+                        <p>asdf</p>
+                                                    <a href="index.php" data-transition="fade">bijdragen</a>
+                    </div>
+                    <div data-role="collapsible" data-collapsed="true">
+                        <h3>goederentreinwagno</h3>
+                        <p>woorddetails</p>
+                    </div>
+                </div>
+                <form name="bijdrage" action="correctie.php" method="POST">
+                        <div data-role="fieldcontain">
+                                <input name="base" id="correctie" placeholder="" value="goederentreinwagon" type="text" />
+                        </div>
+                <div class="ui-grid-a">
+                    <div class="ui-block-a">
+                        <input data-theme="b" value="Sla over" type="submit" />
+                    </div>
+                    <div class="ui-block-b">
+                        <input data-theme="b" value="Dien in" type="submit" />
+                    </div>
+                </div>
+                </form>
+                <div data-role="collapsible-set" data-theme="e" data-content-theme="e">
+                    <div data-role="collapsible" data-collapsed="true">
+                        <h3>
+                            Fout melden
+                        </h3>
+                        <form action="correctie.php" method="POST">
+                        <div data-role="fieldcontain">
+                                <input name="woord" id="woord" placeholder="" value="Nederlands" type="hidden" />
+                                <textarea name="opmerking" id="opmerking" placeholder="" value="" type="textarea">De fout goederentreinwagno </textarea>
+                        </div>
+                        <input data-theme="e" value="Meld fout" type="submit" />
+                        </form>
+                    </div>
+            </div>
+        </div>
+        <script>
+            //App custom javascript
+        </script>
+    </body>
+</html>
---
+++ meervoud.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="content-type" content="text/html;charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>
+        </title>
+        <link rel="stylesheet" href="jquery.mobile-1.2.0.min.css" />
+        <link rel="stylesheet" href="my.css" />
+        <style>
+            /* App custom styles */
+        </style>
+        <script src="jquery.min.js">
+        </script>
+        <script src="jquery.mobile-1.2.0.min.js">
+        </script>
+        <script src="my.js">
+        </script>
+    </head>
+    <body OnLoad="document.bijdrage.meervoud.focus();">
+        <!-- Home -->
+        <div data-role="page" id="page1">
+            <div data-role="content">
+                <div data-role="collapsible-set" data-theme="b">
+                    <div data-role="collapsible" data-collapsed="false">
+                        <h3>
+                            Meervoud bijdragen
+                        </h3>
+                        <p>asdf éß</p><!--FIXME cjharset default charset apache-->
+                                                    <a href="index.php" data-transition="fade">bijdragen</a>
+                    </div>
+                    <div data-role="collapsible" data-collapsed="true">
+                        <h3>goederentreinwagon</h3>
+                        <p>woorddetails</p>
+                    </div>
+                </div>
+                <form name="bijdrage" action="meervoud.php" method="POST">
+                        <div data-role="fieldcontain">
+                                <input name="base" id="meervoud" placeholder="" value="goederentreinwagons" type="text" />
+                        </div>
+<div data-role="fieldcontain">
+<input id="radio0" name="" value="" type="radio" checked>
+<label for="radio0">Eén of meer meervoudsvormen, gescheiden door ;</label><!--TODO restore veld-->
+<input id="radio1" name="" value="" type="radio">
+<label for="radio1">Singulare tantum, niet-telbaar enkelvoud</label><!--TODO hide veld-->
+<input id="radio2" name="" value="" type="radio">
+<label for="radio2">Plurale tantum, niet-telbaar meervoud</label><!--TODO hide veld-->
+</div>
+
+                <div class="ui-grid-a">
+                    <div class="ui-block-a">
+                        <input data-theme="b" value="Sla over" type="submit" />
+                    </div>
+                    <div class="ui-block-b">
+                        <input data-theme="b" value="Dien in" type="submit" />
+                    </div>
+                </div>
+                </form>
+                <div data-role="collapsible-set" data-theme="e" data-content-theme="e">
+                    <div data-role="collapsible" data-collapsed="true">
+                        <h3>
+                            Fout melden
+                        </h3>
+                        <form action="meervoud.php" method="POST">
+                        <div data-role="fieldcontain">
+                                <input name="woord" id="woord" placeholder="" value="Nederlands" type="hidden" />
+                                <textarea name="opmerking" id="opmerking" placeholder="" value="" type="textarea">Het meervoud van goederentreinwagon </textarea>
+                        </div>
+                        <input data-theme="e" value="Meld fout" type="submit" />
+                        </form>
+                    </div>
+            </div>
+        </div>
+        <script>
+            //App custom javascript
+        </script>
+    </body>
+</html>
